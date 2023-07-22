a7c90d33593142905afded4e232c6fdf178f8b54
Samet Çiftçi <temasictfic@gmail.com>
2023-07-22 19:49:20 +0300

    shaped but there is error on Server.cpp

--- CMDS/Notice.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server.hpp"
-
-void Server::Notice(class Client & server, std::vector<const std::string&> params)
-{
-/*     if(params[0][0] == '#')
-        sendServerToChannel(bot, params[0]);
-    else
-        sendServerToClient() */
-}
--- CMDS/Ping.cpp
+++
@@ -1,8 +0,0 @@
-#include "Server.hpp"
-
-//ERR_NEEDMOREPARAMS(Command)
-
-void Server::Ping(Client &client, std::vector<const std::string&>)
-{
-    sendServerToClient(client, "Pong");
-}
